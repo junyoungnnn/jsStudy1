aff02c26e32c32cf438d604dbe2797c5792ee9fb
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-01-23 02:00:00 +0900

    완료

--- jsStudy1/js/todo.js
+++ jsStudy1/js/todo.js
@@ -2,24 +2,29 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
-const toDos = [];
+const TODOS_KEY  = "todos";
+
+let toDos = [];
 
 function saveToDos()
 {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event)
 {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(toDo => toDo.id != parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo)
 {   
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteToDo);
@@ -34,9 +39,39 @@ function handleToDoSubmit(event)
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObject = 
+    {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObject);
+    paintToDo(newTodoObject);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+console.log(savedToDos);
+
+function sayHello(item)
+{
+    console.log("hello", item);
+}
+
+if(savedToDos != null)
+{
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
+function sexyFilter(item)
+{
+    return item != 3
+}
+
+[1,2,3,4].filter(sexyFilter);
+
+sexyFilter(4);
---
+++ jsStudy1/js/weather.js
@@ -0,0 +1,26 @@
+const API_KEY = "e501c74b8e33cc5b9c7fb4870f70a23a";
+
+function onGeoOk(position)
+{
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+
+function onGeoError()
+{
+    alert("Can find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
